314631bef18810987a0c76d0bb2b385a3440cdb6
nmathewa <66082244+nmathewa@users.noreply.github.com>
2021-06-15 12:18:55 +0530

    Update main.py

--- scripts/mom6nma/main.py
+++ scripts/mom6nma/main.py
@@ -6,7 +6,7 @@ test package to set up the input datasets for MOM6 ocean model
 import os
 from grid_create import create_lat_lon_regular
 
-os.chdir("/home/mathew/hdd/UBU20/hdd/IISC_PA/Tools/mom6nma/")
+#os.chdir("/home/mathew/hdd/UBU20/hdd/IISC_PA/Tools/mom6nma/")
 
 x1lon = 88
 x2lon = 92
